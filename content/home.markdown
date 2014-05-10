0ab44449e833682955172d43ed59149de05d3a01
Brad Cerasani <bradcerasani@gmail.com>
2014-05-10 12:27:42 -0500

    initial commit

---
+++ content/home.markdown
@@ -0,0 +1,13 @@
+---
+title: Post Title
+author: Brad
+tags: general
+---
+
+This is a post about nothing.
+
+1. list item one
+2. list item two
+3. list item three
+
+**wow such markdown**
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var ssg = require('gulp-ssg');
+var frontmatter = require('gulp-front-matter');
+var marked = require('gulp-marked');
+var fs = require('fs');
+var es = require('event-stream');
+var mustache = require('mustache');
+var http = require('http');
+var ecstatic = require('ecstatic');
+
+var site = {
+  title: 'My Site'
+};
+
+gulp.task('html', function() {
+  var template = String(fs.readFileSync('templates/page.html'));
+
+  return gulp.src('content/**/*.markdown')
+    .pipe(frontmatter({
+      property: 'meta'
+    }))
+    .pipe(marked())
+    .pipe(ssg(site, {
+      property: 'meta'
+    }))
+    .pipe(es.map(function(file, cb) {
+      var html = mustache.render(template, {
+        page: file.meta,
+        site: site,
+        content: String(file.contents)
+      });
+      file.contents = new Buffer(html);
+      cb(null, file);
+    }))
+    .pipe(gulp.dest('public/'));
+});
+
+gulp.task('watch', function() {
+  http.createServer(
+    ecstatic({root: __dirname + '/public/home'})
+  ).listen(4000);
+  console.log("Preview at http://localhost:4000");
+
+  gulp.watch('content/*.markdown', ['html']);
+  gulp.watch('templates/*.html', ['html']);
+});
+
+
